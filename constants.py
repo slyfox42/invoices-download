880a69367597aa9a312f42aa8054d849677ac323
Federico <federico.obialero@gmail.com>
2019-07-29 17:44:44 +0200

    fixed constants file

--- constants.py
+++ constants.py
@@ -14,7 +14,7 @@ INVOICES_MAIL_FOLDER = os.getenv("INVOICES_MAIL_FOLDER", "")
 
 SSH_HOSTNAME = os.getenv("SSH_HOSTNAME", "")
 SSH_USERNAME = os.getenv("SSH_USERNAME", "")
-SSH_PASSWORD = os.getenv("SSH_PASSWORD", "")
+SSH_KEY_PATH = os.getenv("SSH_KEY_PATH", "")
 
 SCP_BASE_PATH = os.getenv("SCP_BASE_PATH", "")
 
